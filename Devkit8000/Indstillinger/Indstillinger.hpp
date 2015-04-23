00e355ab578b5d85a6f73ca23d1766b3f88acfcc
David Jensen <entvex@gmail.com>
2015-04-23 11:35:47 +0200

    Added methods to set the system time and get it. and Year was added to DateStruct.hpp

--- Devkit8000/Indstillinger/Indstillinger.hpp
+++ Devkit8000/Indstillinger/Indstillinger.hpp
@@ -5,9 +5,16 @@
  *      Author: dje
  */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
 #include "DateStruct.hpp"
 #include "Plant.hpp"
-#include <string>
+
 
 using namespace std;
 
@@ -19,7 +26,7 @@ class Indstillinger {
 			Warning = false;
 			daily = false;
 			Varmelegeme = false;
-			blæserne = false;
+			bloeserne = false;
 			monitor = false;
 			regulering = false;
 		}
@@ -67,21 +74,56 @@ class Indstillinger {
 		this->E_mails[2] = mail3;
 	}
 
-	void GetHardware(bool &Varmelegeme, bool &blæserne)
+	void GetHardware(bool &Varmelegeme, bool &bloeserne)
 	{
 		Varmelegeme = this->Varmelegeme;
-		blæserne = this->blæserne;
+		bloeserne = this->bloeserne;
 	}
 
-	void SetHardware(const bool Varmelegeme, const bool blæserne)
+	void SetHardware(const bool Varmelegeme, const bool bloeserne)
 	{
 		this->Varmelegeme = Varmelegeme;
-		this->blæserne = blæserne;
+		this->bloeserne = bloeserne;
 	}
 
-	void SetDateTime(Date time)
+	//Note This code is Devkit8000 specific and needs root access to run.
+	void setDate(Date time)
 	{
-		//TODO time class
+		string prepCommand;
+		std::string pwd = exec("pwd"); // get current path
+
+		//looks for the return char and removes it
+		if (!pwd.empty() && pwd[pwd.length()-1] == '\n') {
+		    pwd.erase(pwd.length()-1);
+		}
+		//concatenate string.
+		prepCommand += pwd + string("/setTimeBeagleBoard.sh ") + string(time.Year) + string(" ");
+//				string(time.Month) + string(" ") + string(time.Day) + string(" ") + string(time.Hour) +
+//				string(" ") + string(time.Min);
+
+		cout << prepCommand << endl; //Debug
+		const char * command = prepCommand.c_str();
+		std::string res = exec(command);
+		cout << res << endl; //Debug
+	}
+
+
+	Date getDate()
+	{
+		  time_t mytime = time(0);
+		  struct tm* tm_ptr = localtime(&mytime);
+		  struct Date date;
+		  if (tm_ptr)
+		  {
+
+			  date.Min = tm_ptr->tm_min;
+			  date.Hour = tm_ptr->tm_hour;
+			  date.Day = tm_ptr->tm_mday;
+			  date.Month = tm_ptr->tm_mon+1;
+		      date.Year = tm_ptr->tm_year+1900;
+		      return date;
+		  }
+		  return date;
 	}
 
 	void GetNotifications(bool &daily, bool &Warning)
@@ -93,7 +135,7 @@ class Indstillinger {
 	void SetNotifications(const bool &daily, const bool &Warning)
 	{
 		this->Varmelegeme = Varmelegeme;
-		this->blæserne = blæserne;
+		this->bloeserne = bloeserne;
 	}
 
 	// returns 1 If the selected plant is active
@@ -111,13 +153,28 @@ class Indstillinger {
 		return -1;
 	}
 
+
 private:
 		Plant virtuallePlants[6]; 
 		string E_mails[3];        
 		bool Warning;
 		bool daily;
 		bool Varmelegeme;
-		bool blæserne;
+		bool bloeserne;
 		bool monitor;
 		bool regulering;
+
+		string exec(const char* cmd) {
+			//Make a linux shell with read
+		    FILE* pipe = popen(cmd, "r");
+		    if (!pipe) return "ERROR"; //Check if i got pipe
+		    char buffer[128];
+		    string result = "";
+		    while(!feof(pipe)) { //While output read it.
+		    	if(fgets(buffer, 128, pipe) != NULL)
+		    		result += buffer;
+		    }
+		    pclose(pipe); //Close the shell
+		    return result;
+		}
 };
--- Devkit8000/structs/DateStruct.hpp
+++ Devkit8000/structs/DateStruct.hpp
@@ -3,8 +3,9 @@
 
 struct Date
 {
-	int Min;
-	int Hour;
-	int month;
-	int Day;
+	int Min = 0;
+	int Hour = 0;
+	int Day = 0;
+	int Month = 0;
+	int Year = 0;
 };
